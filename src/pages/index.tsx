b22bcc2f0dc1fdbc786d9e8a88c9c91d7afe5e6c
Faisal Jamil <faisaljamilcs@gmail.com>
2022-04-03 02:10:58 +0530

    fix: folder restructure

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <h1>
+        Welcome to <a href='#'>Xelo!</a>
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
